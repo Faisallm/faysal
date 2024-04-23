e6bb8eed05b15a09c4351ffb3c547974e2557716
Faisal Lawan Muhammad <Faisallawan1997@gmail.com>
2024-04-23 10:32:18 -0700

    coder coder fruit

---
+++ faisal.py
@@ -0,0 +1,133 @@
+if __name__ == "__main__":
+    print("""
+Faisal Lawan Muhamamd
+Structural/Machine Learning Engineer
++234-7039071981 | Faisallawan69@gmail.com | https://starlit-nasturtium-523ec5.netlify.app/ | github.com/Faisallm
+
+
+          Education
+1) King Fahd University of Petroleum and Minerals
+Dhahran, Saudi Arabia
+Masters of Science in Structural Engineering
+Aug 2022 - Present
+• GPA: 3.75/4.00
+2) Bayero University Kano
+Kano, Nigeria
+Bachelor of Engineering in Civil Engineering
+Jan. 2016 - Sept. 2021
+• GPA: 4.80/5.00
+• Best Graduating Student Department of Civil Engineering
+• Best Graduating Student Faculty of Engineering
+• Top 10 Graduate Engineers in Nigeria 2021, Nigerian Society of Engineers (NSE)
+• Gifted Scholars Undergraduate Scholarship by SNEPCo division of the Royal Dutch Shell.
+
+
+          Relevant CourseWork
+Mechanics of composite materials, Advanced Structural Mechanics, Advance Structural Analysis and Vibration, Concrete Materials, Reinforced Concrete design and Behaviour, steel structures, determination of bearing capacity from SPT and CPT tests, analysis of slope instability, Gantt Charts, Network Diagrams (Activity on the arrow, Activity on the Node, Precedence Diagram), Linear programming for resource optimization, Benefit-cost analysis, Design of Traffic light control systems, Statistics and Hydrological Forecasting.
+
+
+          Industrial Experience
+1) Civil 3D Engineer
+2021 – 2022
+Wuro Aviation Engineering
+Kano, Nigeria
+• Worked with a senior Engineer from the Aviation ministry in creating the Civil 3D model for the Wachakal airstrip, which is a $15.25 million dollars project with a due date of 31st December, 2023 in Yobe State Nigeria.
+
+• Designed the runway, runway strip and, drainage using Civil 3D software.
+
+2) Design of Structural Elements Intern
+Aug. 2019 – Jan. 2020
+UKM Civil Engineering Consultants
+Kano, Nigeria
+• Designed Structural Elements such as slab, beams, columns and foundations of residential houses for Clients under the supervision experienced Structural Engineers.
+
+• Participated in the setting out of construction as well as in the planning and scheduling phase using tools such as Activity on the arrow, Activity on the Node, Linear Programming for resource optimization, Benefit-cost Analysis, and Critical Path Method (CPM).
+
+• Learned how to do Building Information Modelling (BIM) with REVIT and carry out structural analysis and design using ProtaStructure.
+
+Student Engineer Intern
+Oct 2018 - Dec 2018
+China Civil Engineering Construction Company (CCECC)
+Kano, Nigeria
+• Conducted Soil Tests such Moisture Content Test, Specific Gravity Test, Particle Size Distribution Test, and California Bearing Ratio Test.
+
+• Assisted with inspection for the Kano State Ministry of Works and Housing for the Construction of a 1km flexible pavement highway.
+
+
+
+
+          Projects
+1) EulerAI | python, django, javascript, pandas, numpy, matplotlib, shapely, seaborn Jan 2024 – present
+EulerAI
+• Cloud Platform that democratize access to Artificial Intelligence and Machine Learning Techniques for researchers in academia using simple no-code click and play system.
+
+• Simple interface that allows training of shallow and deep machine learning models, carry out explorative data analysis, question your spreadsheet file and research papers from science direct, data cleaning and data augmentation.
+
+• Access to cloud jupyter-lab environment to carry out analysis, visualization and programming in python, R, sqlite with no need to setup local environment.
+
+• 700+ users
+2) Pymeso | python, numpy, matplotlib, shapely, seaborn
+Aug 2023 – present
+pymeso.py
+• This package provide researchers in the area of mesoscale modelling for concrete, composite materials to easily create mesoscale models. You will need numpy, matplotlib, shapely
+• An Open source 2D mesoscale modelling of concrete python library that was hosted on pypi (Python Package Index) for the generation of Mesoscale Model using the popular take-and-place method.
+
+Determination of Tricycle’s Proportion in Kano Metropolis | Final year project March 2021 – July 2021
+• A 5-month length traffic collection survey which showed that tricycles proportion have increased from 20 percent to 68 percent in the last 10 years
+3) Rock Discontinuity | Python, Fast.ai
+April 2022
+rock.ai
+• A fast.Ai Computer Vision Multiclass Classification model. It classifies between three types of Rock Discontinuity such as folds, bedding planes, and joints.
+
+• It was implemented using python and fast.ai, which is a layer of abstraction above the pytorch framework.
+
+4) Software Implementation of Kani’s Method | Python
+Jan 2020
+kani.py
+• A python code implementation for kani’s method of Structural Analysis (Non-Sway) 
+5) Sonar | Python
+Jan 2020
+sonar.ai
+• A neural network trained to classify between sonar signals bounced off a metal cylinder and those bounced off a rock.
+
+• 85 percent classification accuracy was achieved.
+
+
+          Publications
+A Review of the Parametric Algorithms for the Generation of Continuum Mesoscale Models for Cementitious Composites of High Aggregate Content
+under review
+View Paper
+Influence of Bonded Mortar on Recycled Aggregate Concrete Properties: A review under review
+View Paper
+SYNERGETIC EFFECTS OF NANO-MATERIALS IN CEMENT BASED MATERIALS-A REVIEW
+under review
+View Paper
+
+
+
+          Technical Skills
+Languages: Python, C++, Matlab, Javascript, Dart, Latex, Mathematica
+Frameworks: Django, fastapi, reactjs, pandas, numpy, pytorch, keras, tensorflow Developer Tools: Git, Visual Studio Code, Digital Ocean, Colab, Jupyter Notebook ApplicationsComsol, Abaqus 3D, Ansys, Revit, MS Office
+Volunteering
+Team Lead for Student Committee that successfully organised the 2nd Oil and Gas careers conference at Bayero University Kano. A conference with over 300 attendants including leaders and professionals from oil and gas industries such as: Shell SPDC, SNEPCo, ExxonMobil, NNPC, Schlumberger, NCDMB, members of Academia, Leaders of Engineering regulatory bodies such as Nigerian society of engineers(NSE), COREN and students of the university. The program led to laying a strong foundation for collaboration between the University and NCDMB alongside many students getting internship in the Oil and Gas industry.
+
+Awards
+• Best Graduating Student in Civil Engineering and in the Faculty of Engineering for Academic Session 2019/2020.
+
+• Received the Academic Excellence SNEPCo (Shell Nigeria Exploration and Production Company) Scholarship for students from the departments of Engineering and Medicine from SNEPCo Nigeria, a subsidiary of Shell Nigeria.
+
+• Top 10 Best Engineering Graduates in Nigeria in the Year 2021 by the Nigerian Society of Engineers (NSE)
+
+
+
+          Document Outline
+
+Education
+Industrial Experience
+Projects
+Publications
+Technical Skills
+
+
+
+""")
